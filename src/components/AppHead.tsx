30687c723c4d61b661efc4c98c8221dbe629f62e
Addin Munawwar <moonawar@protonmail.com>
2024-02-29 00:46:01 +0700

    feat: init program detail page, more style in tailwind

--- src/components/AppHead.tsx
+++ src/components/AppHead.tsx
@@ -2,12 +2,12 @@ import sekolahmuHorizontal from '../assets/sekolahmu_horizontal.png';
 
 const AppHead = () => {
     return (
-        <>
-            <div className="w-full h-20 flex justify-center items-center shadow-medium fixed z-50" >
+        <header>
+            <div className="w-full h-20 flex justify-center items-center shadow-medium fixed z-50 bg-white">
                 <img src={sekolahmuHorizontal} alt="Sekolahmu" className="h-10" />
             </div>
             <div className="h-20"/> {/* whitespace for next component */}
-        </>
+        </header>
     );
 }
 
--- src/pages/ProgramDetail.tsx
+++ src/pages/ProgramDetail.tsx
@@ -1,7 +1,65 @@
-const ProgramDetail = () => { 
+const PROGRAM_DUMMY = {
+    id: 1,
+    slug: 'matematika-sd-kelas-1',
+    judul: 'Matematika SD Kelas 1',
+    imgUrl: 'https://images.unsplash.com/photo-1613563696485-f64240817218?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    tujuan_pembelajaran: [
+        'Mengembangkan keterampilan berhitung interaktif',
+        'Mendorong kreativitas dalam mengenali bentuk geometris',
+        'Menumbuhkan pemahaman tentang konsep pengukuran',
+    ],
+    kompetensi: [
+        'Bekerja sama', 'Mandiri', 'Komunikatif', 'Kreatif', 'Berpikir Kritis'
+    ],
+    periode_pengajaran: {
+        tahun_ajaran: '2023/2024',
+        semester: '1',
+    },
+    topik_pembelajaran: [
+        {
+
+        },
+    ]
+}
+
+const fetchProgramData = () => { 
+    // TODO : Fetch data from Backend
+    return PROGRAM_DUMMY;
+}
+
+const ProgramBanner = (props : {imgUrl : string, judul : string}) => {
     return (
-        <div className='h-screen w-screen flex justify-center items-center'>
-            <p className='text-3xl text-black'>Program Detail</p>
+        <div>
+            <img 
+                src={props.imgUrl} 
+                alt={`Banner ${props.judul}`}
+                className="w-full h-60 object-cover"
+            />
+            <div className="w-fit bg-cobalt6 px-5 py-2 shadow-medium -translate-y-1/2 z-20">
+                <h1 className="font-bold text-program-title text-text-100">{props.judul}</h1>
+            </div>
+        </div>
+    );
+}
+
+const ProgramDetail = () => { 
+    const data = fetchProgramData();
+    return (
+        <div className="flex flex-col">
+            <ProgramBanner imgUrl={data.imgUrl} judul={data.judul}/>
+            <div className="px-5">
+                <h2 className="font-semibold text-heading-4 text-text-100">Tujuan Pembelajaran</h2>
+                <ul className="flex flex-col list-disc ml-4 my-5">
+                { 
+                    data.tujuan_pembelajaran.map((tujuan, index)  => {
+                        return <li className="text-lg text-neutral-900" key={index}>
+                            {tujuan}
+                        </li>
+                    })
+                }
+                </ul>
+                <h2 className="font-semibold text-heading-4 text-text-100">Kompetensi</h2>
+            </div>
         </div>
     );
 }
