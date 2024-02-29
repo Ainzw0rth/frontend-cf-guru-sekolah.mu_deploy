3b824264fe7af152e6b94367608e6e45315b0b66
Addin Munawwar <moonawar@protonmail.com>
2024-02-29 11:59:13 +0700

    feat: responsive page placeholder

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
     return (
-        <div className='h-96 w-screen flex justify-center items-center'>
+        <div className='h-96 w-full flex justify-center items-center'>
             <p className='text-3xl text-black'>Home</p>
         </div>
     );
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,6 @@
 const Profile = () => {
     return (
-        <div className='h-96 w-screen flex justify-center items-center'>
+        <div className='h-96 w-full flex justify-center items-center'>
             <p className='text-3xl text-black'>Profile</p>
         </div>
     );
