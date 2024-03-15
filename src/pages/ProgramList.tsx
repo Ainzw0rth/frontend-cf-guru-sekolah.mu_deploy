0752cd67d7c965641464a59ccdccdcd165df9225
Husnia Munzayana <13521077@std.stei.itb.ac.id>
2024-03-15 08:30:54 +0700

    feat: search program list

--- src/pages/ProgramList.tsx
+++ src/pages/ProgramList.tsx
@@ -1,3 +1,4 @@
+import {ChangeEvent, KeyboardEvent, useState} from 'react';
 import ProgramCard from "../components/ProgramCard";
 import { Program } from "../types/Program";
 
@@ -25,28 +26,28 @@ const programs: Program[] = [
     },
     {
         id: 4,
-        title: 'Program 3',
+        title: 'Program 4',
         semester: 1,
         academic_year: '2022/2023',
         imageUrl: 'https://via.placeholder.com/300',
     },
     {
         id: 5,
-        title: 'Program 3',
+        title: 'Program 5',
         semester: 1,
         academic_year: '2022/2023',
         imageUrl: 'https://via.placeholder.com/300',
     },
     {
         id: 6,
-        title: 'Program 3',
+        title: 'Program 6',
         semester: 1,
         academic_year: '2022/2023',
         imageUrl: 'https://via.placeholder.com/300',
     },  
     {
         id: 7,
-        title: 'Program 3',
+        title: 'Program 7',
         semester: 1,
         academic_year: '2022/2023',
         imageUrl: 'https://via.placeholder.com/300',
@@ -55,13 +56,54 @@ const programs: Program[] = [
   ];
 
 const ProgramList = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [filteredPrograms, setFilteredPrograms] = useState(programs);
+
+    const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(e.target.value);
+    }
+
+    const handleSearchClick = () => {
+        filterPrograms(searchTerm);
+    }
+
+    const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key == 'Enter') {
+            filterPrograms(searchTerm);
+        }
+    };
+
+    const filterPrograms = (searchValue: string) => {
+        const filtered = programs.filter(program => 
+            program.title.toLowerCase().includes(searchValue.toLowerCase())
+        );
+        setFilteredPrograms(filtered);
+    };
+
     return (
-        <div className='grid grid-cols-2 gap-4 overflow-y-auto'>
-            {programs.map((program) => (
-                <ProgramCard key={program.id} program={program} />
-            ))}
-        </div>
-    );
+        <div className='bg-neutral8 h-svh w-full justify-center items-center p-10'>
+            <h1 className='font-bold text-program-title text-text-100'>Daftar Program</h1>
+            <div className="flex mb-5">
+                <input
+                    type="text"
+                    placeholder="Cari Program"
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                    onKeyDown={handleKeyDown}
+                    className="flex-1 mr-2"
+                    style={{ border: "1px solid #1890ff", borderRadius: "8px", paddingLeft: "10px", marginRight: "8px" }}
+                />
+                <button onClick={handleSearchClick}           className="bg-persian-blue-500 text-white py-3 rounded-md w-20 text-label-5 font-semibold">
+                    Cari
+                </button>
+            </div>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 overflow-y-auto'>
+        {filteredPrograms.map((program) => (
+            <ProgramCard key={program.id} program={program} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default ProgramList;
